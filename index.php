f035f4f3df684e547ca792866f63c860b7931f78
WAYNE GU <wayne92gu@gmail.com>
2015-05-04 18:55:39 +0800

    date 2015.5.4

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <script src="js/jsFunctions.js"></script>
     <script src="js/Chart.js"></script>
 </head>
-<body contenteditable="false" style=" font-family: '微软雅黑', sans-serif">
+<body contenteditable="false" style=" font-family:, sans-serif">
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse"
@@ -41,16 +41,16 @@
     </nav>
     <div class="container" style="background-color: #ffffff">
         <h1 class="text-center" style="color: black;
-         font-style: normal; font-family: '微软雅黑', sans-serif">
+         font-weight:bold; font-family: 'Agency FB', sans-serif">
            Reviews' Opinion-mining System  <br><br>
         </h1>
         <div class="row">
             <form name="input_url" method="post" action="" onsubmit="checkForm();return false;">
                 <div class="col-lg-12" >
                     <div class="input-group input-group-lg" style="width: 100%" >
-                        <input class="form-control " type="url" name="url" id="url" required="required" placeholder="input your url begin with 'http(s)://' to analysis...">
+                        <input class="form-control " type="url" name="url" id="url" required="required" style="border-Radius:0" autocomplete="off" placeholder="input a url like 'http(s)://item.taobao.com/...'">
                         <span class="input-group-btn">
-                             <button class="btn btn-success" type="submit" id="submit" name="submit" value="post" >
+                             <button class="btn btn-success" type="submit" id="submit" name="submit" value="post" style="border-Radius:0">
                                  <span class="glyphicon glyphicon-search " aria-hidden="true" id="search"></span> Dig!
                              </button>
                         </span>
@@ -63,67 +63,17 @@
 
         </div>
     </div>
-    <script>
-        var tabs=
-            "<ul class='nav nav-tabs'>"+
-            "<li class='active'><a href='#panel-rate' data-toggle='tab'>Rating</a></li>"+
-            "<li><a href='#panel-reviews' data-toggle='tab'>Reviews</a></li>"+
-            "</ul>"+
-            "<div class='tab-content'>"+
-            "<div class='tab-pane fade in active' id='panel-rate' style='background-color: ghostwhite; height: 340px'></div>"+
-            "<div class='tab-pane fade' id='panel-reviews'>第二部分内容.</div>"+
-            "</div>";
-        $("#resultArea").html(tabs);
-        var tab_content=
-            "<br><div id='thumb-div' style=' padding-left: 10px'><a href='#' class='thumbnail thumbnail-sm'><img id='thumb' src='T1WK.jpg' alt='缩略图'></a></div>"+
-            "<div id='rating-bar-div'>"+
-                "<div style='float: left;'>"+
-                    "<div class='rating-label'>物流：</div>"+
-                    "<div class='rating-label'>价格：</div>"+
-                    "<div class='rating-label'>服务：</div>"+
-                    "<div class='rating-label'>发货：</div>"+
-                    "<div class='rating-label'>包装：</div>"+
-                    "<div class='rating-label'>质量：</div>"+
-                    "<div class='rating-label'>款式：</div>"+
-                    "<div class='rating-label'>尺寸：</div>"+
-                    "<div class='rating-label'>做工：</div>"+
-                    "<div class='rating-label'>面料：</div>"+
-                "</div>"+
-                "<div style='float: right;padding-right: 10px'>"+
-                    "<div id='rate_1' class='rating-label'>xxx</div>"+
-                    "<div id='rate_2' class='rating-label'>xxx</div>"+
-                    "<div id='rate_3' class='rating-label'>xxx</div>"+
-                    "<div id='rate_4' class='rating-label'>xxx</div>"+
-                    "<div id='rate_5' class='rating-label'>xxx</div>"+
-                    "<div id='rate_6' class='rating-label'>xxx</div>"+
-                    "<div id='rate_7' class='rating-label'>xxx</div>"+
-                    "<div id='rate_8' class='rating-label'>xxx</div>"+
-                    "<div id='rate_9' class='rating-label'>xxx</div>"+
-                    "<div id='rate_10' class=rating-label'>xxx</div>"+
-                "</div>"+
-                "<div id='bars-area' style='float: right;padding-top: 3px'></div>"+
-            "</div>";
-        $("#panel-rate").html(tab_content);
-        var bars="";
-        for(var i=1;i<11;i++){
-                bars=bars+
-                    "<div id='progress' class='progress progress-striped rating-bar'>"+
-                        "<div id='bar_"+i+"' class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100' style='width: 43.2569%;'></div>"+
-                    "</div>";
-            }
-        $("#bars-area").html(bars);
-    </script>
     <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
         <div class="text-center" >
             <span style="color: gray">
-                E.E.Dept Fudan University Shanghai China<br>WebSite Ver.0.1 by Webstorm
+                E.E.Dept Fudan University Shanghai China<br>Copyright © 2015
             </span>
         </div>
     </nav>
     <!-- 模态框（Modal） -->
     <div class="modal fade" id="about" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
-            <div class="modal-content">
+            <div class="modal-content" style="border-Radius:0;">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"
                             aria-hidden="true">×
--- js/jsFunctions.js
+++ js/jsFunctions.js
@@ -3,17 +3,68 @@
  */
 function showResult(resp){
     var results = eval("("+resp+")");
-
-
+    var tabs=
+        "<ul class='nav nav-tabs'>"+
+        "<li class='active'><a href='#panel-rate' data-toggle='tab' style='border-Radius:0;'>Rating</a></li>"+
+        "<li><a href='#panel-reviews' data-toggle='tab' style='border-Radius:0;'>Reviews</a></li>"+
+        "</ul>"+
+        "<div class='tab-content'>"+
+        "<div class='tab-pane fade in active' id='panel-rate' style='background-color: ghostwhite; height: 380px'></div>"+
+        "<div class='tab-pane fade' id='panel-reviews'>第二部分内容.</div>"+
+        "</div>";
+    $("#resultArea").html(tabs);
+    var tab_content=
+        "<div style='padding:10px;font-size:20px' id='title'></div><div id='thumb-div' style=' padding-left: 20px'><a href='#' class='thumbnail thumbnail-sm'><img id='thumb' src='"+results['pic']+"' alt='缩略图'></a></div>"+
+        "<div id='rating-bar-div'>"+
+        "<div style='float: left;'>"+
+        "<div class='rating-label'>物流：</div>"+
+        "<div class='rating-label'>价格：</div>"+
+        "<div class='rating-label'>服务：</div>"+
+        "<div class='rating-label'>发货：</div>"+
+        "<div class='rating-label'>包装：</div>"+
+        "<div class='rating-label'>质量：</div>"+
+        "<div class='rating-label'>款式：</div>"+
+        "<div class='rating-label'>尺寸：</div>"+
+        "<div class='rating-label'>做工：</div>"+
+        "<div class='rating-label'>面料：</div>"+
+        "</div>"+
+        "<div style='float: right;padding-right: 20px'>"+
+        "<div id='rate_1' class='rating-label'></div>"+
+        "<div id='rate_2' class='rating-label'></div>"+
+        "<div id='rate_3' class='rating-label'></div>"+
+        "<div id='rate_4' class='rating-label'></div>"+
+        "<div id='rate_5' class='rating-label'></div>"+
+        "<div id='rate_6' class='rating-label'></div>"+
+        "<div id='rate_7' class='rating-label'></div>"+
+        "<div id='rate_8' class='rating-label'></div>"+
+        "<div id='rate_9' class='rating-label'></div>"+
+        "<div id='rate_10' class='rating-label'></div>"+
+        "</div>"+
+        "<div id='bars-area' style='float: right;padding-top: 3px'></div>"+
+        "</div>";
+    $("#panel-rate").html(tab_content);
+    $("#title").html(results['title']);
+    var bars="";
+    for(var i=1;i<11;i++){
+        bars=bars+
+        "<div id='progress' class='progress progress-striped rating-bar'>"+
+        "<div id='bar_"+i+"' class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100' style=''></div>"+
+        "</div>";
+    }
+    $("#bars-area").html(bars);
+    for(i=1;i<11;i++){
+        var rate=results['label_p_'+i]*100/results['label_'+i];
+        if(parseInt(rate)!=rate)rate=rate.toFixed(1);
+        $("#rate_"+i).html(rate+"%");
+        $("#bar_"+i).attr("style", "width:"+rate+"%");
+    }
 }
 
 
 function dig(urlId){
-    var n=0;
-    var play;
     var resultArea = document.getElementById("resultArea");
     var button = document.getElementById("submit");
-    var str = "<p><br></p><div class='alert alert-success'><a href='#' class='close' data-dismiss='alert'>&times;</a><strong>Success！</strong>The url has been uploaded successfully, please wait while analysing.</div>";
+    var str = "<div class='alert alert-success' style='border-Radius:0;'><a href='#' class='close' data-dismiss='alert'>&times;</a>The url has been uploaded successfully, please wait while analysing.</div>";
     resultArea.innerHTML = str;
     function display(){
         $.post("getResult.php", {urlId:urlId},
@@ -36,6 +87,13 @@ function checkForm(){
     var insertOK=0;
     var urlId;
     var button = document.getElementById("submit");
+    var input_url = document.getElementById("url").value;
+    var patt = /item\.taobao\.com\//;
+    var danger_str = "<div class='alert alert-danger' style='border-Radius:0;'><a href='#' class='close' data-dismiss='alert'>&times;</a>Input error, please check your url.</div>";
+    if (patt.exec(input_url)==null){
+        $("#resultArea").html(danger_str);
+        return false;
+    }
     button.innerHTML="<span class='glyphicon glyphicon-refresh ' aria-hidden='true' id='refresh'></span> Dig.";
     button.className="btn btn-success disabled";
     if (window.XMLHttpRequest)
@@ -57,7 +115,7 @@ function checkForm(){
         urlId = parseInt(Math.random()*100000);
         xmlhttp.open("POST", "post.php", false);
         xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send("url=" + encodeURIComponent(document.getElementById("url").value) + "&submit=" + document.getElementById("submit").value + "&urlId=" + urlId);
+        xmlhttp.send("url=" + encodeURIComponent(input_url) + "&submit=" + document.getElementById("submit").value + "&urlId=" + urlId);
     }
     send();
     dig(urlId);
